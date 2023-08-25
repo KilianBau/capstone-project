f9997614982041b27020960eba4fd3fef478d136
Kilian Baur <baurkilian@yahoo.de>
2023-08-25 12:43:54 +0200

    try to fix deployment error

--- components/CountryFormPage/CountryForm.test.js
+++ components/CountryFormPage/CountryForm.test.js
@@ -1,5 +1,4 @@
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import CountryForm from ".";
 
 test("calls the onSubmit handler with inputs on submit", async () => {
